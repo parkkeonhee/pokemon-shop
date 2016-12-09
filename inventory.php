0f0d6febecbff01069667fbe17db55089d7b8181
Keon Hee Park <parkkeonhee@gmail.com>
2016-12-09 23:50:40 +0000

    index added

--- inventory.php
+++ inventory.php
@@ -16,9 +16,6 @@
 <body>
 
 
-
-
-
 </body>
 
 </html>
--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <!doctype html>
-
 <!--
     Created by Kenny Nguyen, Keon Hee Park, and Rashidat Akande
     login.php
@@ -7,9 +6,7 @@
 	Instructor: Louis Henry
 	Date: December 12, 2016
 -->
-
 <html lang="en-US">
-
 <head>
     <meta charset="utf-8" />
     <title>User Login Page</title>
@@ -37,5 +34,4 @@ session_destroy();
         <a href="admin-pages/create-account.php" class="button-orange">Create an Account</a>
     </div>
 </body>
-
 </html>

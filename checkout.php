f4ea4efafd63fa42f44a63cf169c29972376055a
Keon Hee Park <parkkeonhee@gmail.com>
2016-12-10 20:59:32 +0000

    added checkout.php

--- checkout.php
+++ checkout.php
@@ -14,9 +14,65 @@
 </head>
 
 <body>
-
-
-
+    
+    <div>
+        <!-- left page div 70%-->
+        <div>
+            <h3>1. Your Shipping Address</h3>
+            
+            <h3>2. Your Billing Address</h3>
+            
+            <h3>3. Payment Method</h3>
+            <form action="" method="post">
+                Card number: <input type="number" name=""/>
+                <br>
+                Name on card: <input type="text" name="ownerNameCC"/>
+                Expiration date:
+                <br>
+                Month:
+                <select name="expirationMonthCC">
+                    <option value="1">January</option>
+                    <option value="2">February</option>
+                    <option value="3">March</option>
+                    <option value="4">April</option>
+                    <option value="5">May</option>
+                    <option value="6">June</option>
+                    <option value="7">July</option>
+                    <option value="8">August</option>
+                    <option value="9">September</option>
+                    <option value="10">October</option>
+                    <option value="11">November</option>
+                    <option value="12">December</option>
+                </select>
+                Year:
+                <select name="expirationYearCC">
+                    <option value="2016">2016</option>
+                    <option value="2017">2017</option>
+                    <option value="2018">2018</option>
+                    <option value="2019">2019</option>
+                    <option value="2020">2020</option>
+                    <option value="2021">2021</option>
+                </select>
+                <br>
+                Year: <input type="" name="expireYearCC"/>
+            </form>
+            
+            
+            <h3>Review Items and Shipping</h3>
+            <!-- need php code here-->
+        </div>
+        <!-- right page div 30%-->
+        <div>
+            <!-- need php or javascript code here-->
+            <h3>Your order</h3>
+            <p>Items:</p>
+            <p>Free Shipping & Handling:</p>
+            <p>Order total:</p>
+            
+            <a href="account.php" class="button">Go back</a>
+        </div>
+    </div>
+    
 
 
 </body>

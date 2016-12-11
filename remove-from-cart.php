1f437ec16567fbc5ecc1276c7d69b2b1a4cd7247
Keon Hee Park <parkkeonhee@gmail.com>
2016-12-11 18:19:23 +0000

    cleanup format

--- remove-from-cart.php
+++ remove-from-cart.php
@@ -16,38 +16,31 @@
 	<body>
 		<div>
 			<?php
-	session_start();
-	$user = $_SESSION['name'];
-
-
-$servername = "localhost";
-$username = "knguyen74";
-$password = "knguyen74";
-$dbname = "knguyen74";
-
-
-
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-$id=$_POST["id"];
-
-$sql = "DELETE from items WHERE
-username = '$user' AND id='$id' AND type='C'";
-if ($conn->query($sql) === TRUE) {
-	echo 'Item is removed';
-} else {
-	echo 'Error: Item failed to be removed';
-}
-
-$conn->close();
-?>
-	<a href="account.php"><input type="button" id="btn1" value="OK"></a>
-	</div>
-</body>
-
+				session_start();
+				$user = $_SESSION['name'];
+				
+				$servername = "localhost";
+				$username = "knguyen74";
+				$password = "knguyen74";
+				$dbname = "knguyen74";
+				
+				// Create connection
+				$conn = mysqli_connect($servername, $username, $password, $dbname);
+				
+				// Check connection
+				if ($conn->connect_error) {
+					die("Connection failed: " . $conn->connect_error);
+				}
+				$id=$_POST["id"];
+				$sql = "DELETE from items WHERE username = '$user' AND id='$id' AND type='C'";
+				if ($conn->query($sql) === TRUE) {
+					echo 'Item is removed';
+				} else {
+					echo 'Error: Item failed to be removed';
+				}
+			$conn->close();
+			?>
+			<a href="account.php"><input type="button" id="btn1" value="OK"></a>
+		</div>
+	</body>
 </html>

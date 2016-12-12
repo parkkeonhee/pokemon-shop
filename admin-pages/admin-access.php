2094159d8f1a5a1a1a593266b020224e3f24babe
Keon Hee Park <parkkeonhee@gmail.com>
2016-12-12 17:32:54 +0000

    css updates

--- admin-pages/admin-access.php
+++ admin-pages/admin-access.php
@@ -14,6 +14,15 @@
 		<title>Admin: Access</title>
 	</head>
 	<body>
+		
+		<form action="../login.php" method="post">
+			<div class="right">
+				<input type="submit" id="btn2" value="Logout" class="button-orange">
+			</div>
+		</form>
+		<div class="center">
+			<img src="../images/pokestop.png" alt="pokestop"/>
+		</div>
 		<?php
 			session_start();
 			$user = $_SESSION['name'];
@@ -28,13 +37,34 @@
 				die("Connection failed: " . $conn->connect_error);
 			}
 			
+			
+			
 			$sql0 = "SELECT username, password, admin, fname, lname, email, phone, sadd, scity, scounty, sstate, szip, badd, bcity, bcounty, bstate, bzip FROM accounts";
 			$result0 = $conn->query($sql0);
-			echo "<h3>ACCOUNTS</h3>";
+			echo "<h2>Accounts</h2>";
 			
 			if ($result0->num_rows > 0) {
 				// output data of each row
-				echo "<table><tr><th>username</th><th>password</th><th>admin</th><th>fname</th><th>lname</th><th>email</th><th>phone</th><th>sadd</th><th>scity</th><th>scounty</th><th>sstate</th><th>szip</th><th>badd</th><th>bcity</th><th>bcounty</th><th>bstate</th><th>bzip</th></tr>";
+				echo "<table>
+						<tr>
+							<th>username</th>
+							<th>password</th>
+							<th>admin</th>
+							<th>fname</th>
+							<th>lname</th>
+							<th>email</th>
+							<th>phone</th>
+							<th>sadd</th>
+							<th>scity</th>
+							<th>scounty</th>
+							<th>sstate</th>
+							<th>szip</th>
+							<th>badd</th>
+							<th>bcity</th>
+							<th>bcounty</th>
+							<th>bstate</th>
+							<th>bzip</th>
+						</tr>";
 				while($row = $result0->fetch_assoc()) {
 				$username=$row["username"];
 				$password=$row["password"];
@@ -54,10 +84,25 @@
 				$bstate=$row["bstate"];
 				$bzip=$row["bzip"];
 				
-				echo "<tr><td>".$username."</td><td>".$password."</td><td>".$admin."</td><td>".$fname."</td>
-						<td>".$lname."</td><td>".$email."</td><td>".$phone."</td><td>".$sadd."</td><td>".$scity."</td>
-						<td>".$scounty."</td><td>".$sstate."</td><td>".$szip."</td><td>".$badd."</td><td>".$bcity."</td>
-						<td>".$bcounty."</td><td>".$bstate."</td><td>".$bzip."</td></tr>";
+				echo "<tr>
+						<td>".$username."</td>
+						<td>".$password."</td>
+						<td>".$admin."</td>
+						<td>".$fname."</td>
+						<td>".$lname."</td>
+						<td>".$email."</td>
+						<td>".$phone."</td>
+						<td>".$sadd."</td>
+						<td>".$scity."</td>
+						<td>".$scounty."</td>
+						<td>".$sstate."</td>
+						<td>".$szip."</td>
+						<td>".$badd."</td>
+						<td>".$bcity."</td>
+						<td>".$bcounty."</td>
+						<td>".$bstate."</td>
+						<td>".$bzip."</td>
+					</tr>";
 				}
 				echo "</table>";
 			} else {
@@ -66,10 +111,20 @@
 			//id, name,description, img,type, price
 			$sql1 = "SELECT id, name, description, img, price, type, stock  FROM products";
 			$result1 = $conn->query($sql1);
-			echo "<h3>PRODUCTS</h3>";
+			
+			echo "<h2>Products</h2>";
 			if ($result1->num_rows > 0) {
 				// output data of each row
-				echo "<table><tr><th>id</th><th>name</th><th>description</th><th>img</th><th>price</th><th>type</th><th>stock</th></tr>";
+				echo "<table>
+						<tr>
+							<th>id</th>
+							<th>name</th>
+							<th>description</th>
+							<th>img</th>
+							<th>price</th>
+							<th>type</th>
+							<th>stock</th>
+						</tr>";
 				while($row = $result1->fetch_assoc()) {
 					$id=$row["id"];
 					$name=$row["name"];
@@ -78,7 +133,15 @@
 					$price=$row["price"];
 					$type=$row["type"];
 					$stock=$row["stock"];
-					echo "<tr><td>".$id."</td><td>".$name."</td><td>".$description."</td><td>".$img."</td><td>".$price."</td><td>".$type."</td><td>".$stock."</td></tr>";
+					echo "<tr>
+							<td>".$id."</td>
+							<td>".$name."</td>
+							<td>".$description."</td>
+							<td>".$img."</td>
+							<td>".$price."</td>
+							<td>".$type."</td>
+							<td>".$stock."</td>
+						</tr>";
 				}
 				echo "</table>";
 			} else {
@@ -87,11 +150,19 @@
 			
 			$sql2 = "SELECT id, number, username, type, date, ordernum FROM items";
 			$result2 = $conn->query($sql2);
-			echo "<h3>items</h3>";
 			
+			echo "<h2>Items</h2>";
 			if ($result2->num_rows > 0) {
 				// output data of each row
-				echo "<table><tr><th>id</th><th>number</th><th>username</th><th>type</th><th>date</th><th>ordernum</th></tr>";
+				echo "<table>
+						<tr>
+							<th>id</th>
+							<th>number</th>
+							<th>username</th>
+							<th>type</th>
+							<th>date</th>
+							<th>ordernum</th>
+						</tr>";
 				
 				while($row = $result2->fetch_assoc()) {
 					$id=$row["id"];
@@ -101,7 +172,14 @@
 					$date=$row["date"];
 					$ordernum=$row["ordernum"];
 					
-					echo "<tr><td>".$id."</td><td>".$number."</td><td>".$username."</td><td>".$type."</td><td>".$date."</td><td>".$ordernum."</td></tr>";
+					echo "<tr>
+							<td>".$id."</td>
+							<td>".$number."</td>
+							<td>".$username."</td>
+							<td>".$type."</td>
+							<td>".$date."</td>
+							<td>".$ordernum."</td>
+						</tr>";
 				}
 				echo "</table>";
 			} else {
@@ -111,7 +189,7 @@
 		?>
 		
 		<form action="admin-create-admin.php" method="post">
-			<h3>add admin</h3>
+			<h2>Add Admin</h2>
 			<div>
 				<label>name:</label> <input name="name" type="text">
 				<br>
@@ -124,7 +202,7 @@
 		</form>
 		
 		<form action="admin-delete-account.php" method="post">
-			<h3>delete user</h3>
+			<h2>Delete user</h2>
 			<div>
 				<label>name:</label> <input name="name" type="text">
 			</div>
@@ -135,7 +213,7 @@
 		</form>
 		
 		<form action="admin-change-password.php" method="post">
-			<h3>change password of account</h3>
+			<h2>Change Password of Account</h2>
 			<div>
 				<label>name:</label> <input name="name" type="text">
 				<br>
@@ -149,7 +227,7 @@
 		</form>
 		
 		<form action="admin-add-product.php" method="post">
-			<h3>add product</h3>
+			<h2>Add Product</h2>
 			<div>
 				<label>id:</label> <input name="id" type="text">
 				<br>
@@ -178,7 +256,7 @@
 		</form>
 		
 		<form action="admin-delete-product.php" method="post">
-			<h3>delete product</h3>
+			<h2>Delete Product</h2>
 			<div>
 				<label>id:</label> <input name="id" type="text">
 			</div>
@@ -189,7 +267,7 @@
 		</form>
 		
 		<form action="admin-update-product-stock.php" method="post">
-			<h3>update product's stock</h3>
+			<h2>Update Product's Stock</h2>
 			<div>
 				<label>id:</label> <input name="id" type="text">
 				<br>
@@ -203,7 +281,7 @@
 		</form>
 		
 		<form action="admin-update-product-description.php" method="post">
-			<h3>update product's description</h3>
+			<h2>Update Product's Description</h2>
 			<div>
 				<label>id:</label> <input name="id" type="text">
 				<br>
@@ -217,16 +295,9 @@
 		</form>
 		
 		<form action="reset-item-table.php" method="post">
-			<h3>Clean Item Table</h3>
+			<h2>Clean Item Table</h2>
 			<div class="sub">
 				<input type="submit" id="btn2" value="Submit" class="button">
-			</div>
-		</form>
-		
-		<form action="../login.php" method="post">
-			<h3>Logout</h3>
-			<div>
-				<input type="submit" id="btn2" value="Logout" class="button-orange">
 			</div>
 		</form>
 </body>

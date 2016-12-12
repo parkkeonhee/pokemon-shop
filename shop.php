d70cf200c9007bd6828a93de9a14ccae9f6cd60a
Keon Hee Park <parkkeonhee@gmail.com>
2016-12-12 14:08:39 +0000

    css updates

--- shop.php
+++ shop.php
@@ -57,62 +57,17 @@
                     $stock=$row["stock"];
                     
                     echo '<form action="item-page.php" method="post">   
-		 <input name="id" class="makehidden" type="text" value="'.$id.'">
-		 <div><img src= "images/pokedex/'.$img.'" alt='.$name.'> '.$name.' '.$type.' '.$price.'
-		 <input type="submit" class="button" value="View"></div>
-		 </form>';
+                            <input name="id" class="makehidden" type="text" value="'.$id.'">
+                            <div>
+                                <img src="images/pokedex/'.$img.'" alt='.$name.'/>
+                                <input type="submit" class="button" value="'.$name.'($'.$priceView.')"/>
+                            </div>
+                        </form>';
                 }
             } else {
                 echo "0 results";
             }
             $conn->close();
             ?>
-<!--
-     top div 30%
-    <div>
-        <div class="one">
-            <a href="login.php" class="button">Cart</a>
-            <a href="shop.php" class="button">Account</a>
-        </div>
-        <div class="two">
-            <img src="images/pokestop.png" alt="pokestop"/>
-        </div>
-    </div>
-    
-    div: rest of bottom page 70% 
-    <div>
-        <div>
-        BOTTOM-LEFT-DIV table 
-        <h4>Refine Search</h4>
-        <form action="" method="post">
-            Price range: <input type="number" name="search-min-price" min="0" max="1000" step="0.01">
-            to <input type="number" name="search-max-price" min="0" max="1000" step="0.01">
-            <input type="submit" class="button" value="Submit"/>
-        </form>
-        <form action="" method="post">
-            Browse Inventory by Name: <input type="search" name="search-name"/>
-            <input type="submit" class="button" value="Submit"/>
-        </form>
-        </div>
-         end of bottom left div table (refine search category) 
-         start of bottom right div table (table pokemon category)
-        <div>
-            <table>
-                <tr>
-                    <th>ID</th>
-                    <th>Image</th>
-                    <th>Name</th>
-                    <th>Price</th>
-                </tr>
-                <td>001</td>
-                <td>IMAGE HERE</td>
-                <td>Bulbasaur</td>
-                <td>$24.70</td>
-            </table>
-        </div>
-        end of bottom right category
-    </div>
-     end of bottom page category 70%
--->
     </body>
 </html>
